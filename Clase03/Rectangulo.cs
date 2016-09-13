68595d6cc7a53b49abe75eb14ff5a99d812feccc
debyconde <ssol_d@hotmail.com>
2016-09-13 20:49:55 -0300

    en clase

---
+++ Clase03/Rectangulo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase03
+{
+    class Rectangulo
+    {
+        private int ladoUno;
+        private int ladoDos;
+        private double perimetro;
+        private double superficie;
+
+        public Rectangulo(int ladoUno, int ladoDos)
+        {
+            this.ladoUno = ladoUno;
+            this.ladoDos = ladoDos;
+            this.ObtenerDatos();
+        }
+        
+        private void ObtenerDatos() 
+        {
+            this.perimetro = (this.ladoUno + this.ladoUno + this.ladoDos + this.ladoDos);
+            this.superficie = this.ladoUno * this.ladoDos;
+        }
+        public string Mostrar()
+        {
+            string ret;
+            ret = "\tRECTANGULO\nEl perimetro es: " + this.perimetro + "\nLa superficie es: " + this.superficie + "\n\n";
+            return ret;
+        }
+
+        public static void Dibujar(Rectangulo r)
+        {
+            string draw = "";
+            for (int i = 0; i < r.ladoDos; i++)
+            {
+                for (int j = 0; j < r.ladoUno; j++)
+                {
+                    draw += "*";
+                }
+                draw += "\n";
+            }
+            Console.Write(draw);
+        }
+
+    }
+}
---
+++ Clase03/Triangulo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase03
+{
+    class Triangulo
+    {
+        private int baseT;
+        private int altura;
+        private double superficie;
+
+        public Triangulo(int baseT, int altura)
+        {
+            this.altura = altura;
+            this.baseT = baseT;
+            this.ObtenerDatos();
+        }
+        
+        
+        private void ObtenerDatos()
+        {
+            this.superficie = (this.baseT * this.altura) / 2;
+        }
+        public string Mostrar()
+        {
+            string ret;
+            ret = "\tTRIANGULO\nLa superficie es: " + this.superficie + "\n\n";
+            return ret;
+        }
+
+        public static void Dibujar(Triangulo t)
+        {
+            string draw = "";
+
+            int height = t.altura;
+            int width = t.baseT;
+
+            int halfWidth = width / 2;
+            int step = halfWidth / height;
+            int distanceToCenter = 0;
+
+            int drawFrom = 0;
+            int drawTo = width;
+            for (int row = 0; row < height; row++)
+            {
+                drawFrom = 0;
+                drawTo = width;
+
+                if (row != height - 1)
+                {
+                    distanceToCenter = row * step;
+                    drawFrom = halfWidth - distanceToCenter;
+                    drawTo = halfWidth + distanceToCenter;
+                }
+
+                for (int c = 0; c < width; c++)
+                {
+                    if (c >= drawFrom || c <= drawTo)
+                    {
+                        draw += "*";
+                    }
+                    else
+                    {
+                        draw += " ";
+                    }
+                }
+
+                draw = "\n";
+            }
+            
+            Console.Write(draw);
+        }
+    }
+}
